130a175e1061788475a54376b9794094670fff86
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2023-10-21 21:38:08 +0000

    Added utils.cpython-38.pyc, run, readme.md; deleted config.yaml; and changes in 2 other files

--- code/run.py
+++ code/run.py
@@ -15,31 +15,35 @@ import utils
 # cudadev=os.system("cat $PBS_GPUFILE | rev | cut -d\"u\" -f1")
 # os.environ['CUDA_VISIBLE_DEVICES'] = str(cudadev)
 # print(cudadev)
-
-DLC_PROJECT_PATH = '/data/universal_eye_tracking-peterl-2019-07-10'
+DATA = pathlib.Path('/root/capsule/data/')
+DLC_PROJECT_PATH = DATA / 'universal_eye_tracking-peterl-2019-07-10'
+VIDEO_SUFFIXES = ('.mp4', '.avi', '.wmv', '.mov')
 
 def get_eye_paths() -> Iterator[pathlib.Path]:
-    for asset in pathlib.Path('/data').iterdir():
-        if asset.name == pathlib.Path(DLC_PROJECT_PATH).name:
+    for path in DATA.iterdir():
+        if path.name == DLC_PROJECT_PATH.name:
             continue
-        if not asset.is_dir():
+        if not path.is_dir():
             continue
-        yield from asset.glob('*behavior*/*[eE]ye*')
+        yield from (p for p in path.glob('*behavior*/*[eE]ye*') if p.suffix() in VIDEO_SUFFIXES)
             
 if __name__ == "__main__":
     # phase 1 
-    video_file_path = next(file for file in get_eye_paths() if file.suffix != '.json')
-    print(video_file_path)
+    video_file_path = next(get_eye_paths())
+    print(video_file_path.as_posix())
 
     # output_file = pathlib.Path('results/') / video_file_path.with_suffix(
     #     'DeepCut_resnet50_universal_eye_trackingJul10shuffle1_1030000.h5'
     #     ).name
 
     # # ### Path to trained model:
-    path_config_file = 'code/config.yaml'
-
     # # ### Track points in video and generate h5 file:
-    deeplabcut.analyze_videos(path_config_file, [video_file_path.as_posix()]) #can take a list of input videos
+    deeplabcut.analyze_videos(
+        config=DLC_PROJECT_PATH / 'config.yaml',
+        videos=[
+          video_file_path.as_posix(),
+        ],
+    )
 
     # os.rename(output_file,output_file_path)
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+DLC conda environment: https://github.com/DeepLabCut/DeepLabCut/blob/main/conda-environments/DEEPLABCUT.yaml
+
+source code for `deeplabcut.analyze_videos()`: https://github.com/DeepLabCut/DeepLabCut/blob/main/deeplabcut/pose_estimation_tensorflow/predict_videos.py#L268
